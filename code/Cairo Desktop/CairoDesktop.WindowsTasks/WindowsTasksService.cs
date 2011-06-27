5728c7184ec026584ba297e400c9ca88ac074bb3
RevolutionSmythe <asdfisbetterthanjkl_AT_gmail.com>
2011-06-26 21:17:53 -0500

    Attempt to get the finding of child windows to work.

--- code/Cairo Desktop/CairoDesktop.WindowsTasks/WindowsTasksService.cs
+++ code/Cairo Desktop/CairoDesktop.WindowsTasks/WindowsTasksService.cs
@@ -83,6 +83,58 @@ namespace CairoDesktop.WindowsTasks
             }
         }
 
+        [DllImport ("user32")]
+        [return: MarshalAs (UnmanagedType.Bool)]
+        public static extern bool EnumChildWindows (IntPtr window, EnumWindowProc callback, IntPtr i);
+
+        /// <summary>
+        /// Returns a list of child windows
+        /// </summary>
+        /// <param name="parent">Parent of the windows to return</param>
+        /// <returns>List of child windows</returns>
+        public static List<IntPtr> GetChildWindows (IntPtr parent)
+        {
+            List<IntPtr> result = new List<IntPtr> ();
+            GCHandle listHandle = GCHandle.Alloc (result);
+            try
+            {
+                EnumWindowProc childProc = new EnumWindowProc (EnumWindow);
+                EnumChildWindows (parent, childProc, GCHandle.ToIntPtr (listHandle));
+            }
+            finally
+            {
+                if (listHandle.IsAllocated)
+                    listHandle.Free ();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Callback method to be used when enumerating windows.
+        /// </summary>
+        /// <param name="handle">Handle of the next window</param>
+        /// <param name="pointer">Pointer to a GCHandle that holds a reference to the list to fill</param>
+        /// <returns>True to continue the enumeration, false to bail</returns>
+        private static bool EnumWindow (IntPtr handle, IntPtr pointer)
+        {
+            GCHandle gch = GCHandle.FromIntPtr (pointer);
+            List<IntPtr> list = gch.Target as List<IntPtr>;
+            if (list == null)
+            {
+                throw new InvalidCastException ("GCHandle Target could not be cast as List<IntPtr>");
+            }
+            list.Add (handle);
+            //  You can modify this to check to see if you want to cancel the operation, then return a null here
+            return true;
+        }
+
+        /// <summary>
+        /// Delegate for the EnumChildWindows method
+        /// </summary>
+        /// <param name="hWnd">Window handle</param>
+        /// <param name="parameter">Caller-defined variable; we use it for a pointer to our list</param>
+        /// <returns>True to continue enumerating, false to bail.</returns>
+        public delegate bool EnumWindowProc (IntPtr hWnd, IntPtr parameter);
         public void GetOpenWindows ()
         {
             //Run through all the processes and see whether we can find anything that we need to add already
@@ -92,8 +144,21 @@ namespace CairoDesktop.WindowsTasks
                 if (proc.MainWindowHandle != IntPtr.Zero)//If the main window isn't null, its a GUI window
                 {
                     var win = new ApplicationWindow (proc.MainWindowHandle, this);
-                    if(win.Title != "")//Make sure that it isn't supposed to be hidden
+                    if(win.Title != "" && win.ShowInTaskbar)//Make sure that it isn't supposed to be hidden
                         Windows.Add (win);
+                    else if (!win.ShowInTaskbar)
+                    {
+                        List<IntPtr> childHandles = GetChildWindows (proc.MainWindowHandle);
+                        foreach (IntPtr childHandle in childHandles)
+                        {
+                            win = new ApplicationWindow (childHandle, this);
+                            if (win.Title != "" && win.ShowInTaskbar)//Make sure that it isn't supposed to be hidden
+                            {
+                                Windows.Add (win);
+                                break;
+                            }
+                        }
+                    }
                 }
             }
         }
